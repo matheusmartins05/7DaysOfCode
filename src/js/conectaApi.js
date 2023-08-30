56e5f098c63d178329e504de1b01635022239e87
matheusmartins05 <matheusmartins2901@gmail.com>
2023-08-30 11:34:11 -0300

    finalidado dia 3

--- src/js/conectaApi.js
+++ src/js/conectaApi.js
@@ -1,11 +1,3 @@
-async function conectarApi() {
-  const api =
-    "https://api.themoviedb.org/3/movie/11?api_key=c5c16f06eea41ae9900c6f3484b51ecc";
-  const apiResponse = await fetch(api);
-
-  return apiResponse;
-}
-
 async function discoverFilm(listaFilmes) {
   const options = {
     method: "GET",
--- src/js/main.js
+++
@@ -1,5 +0,0 @@
-import { loadCard } from "./criaCard.js";
-
-const $body = document.querySelector('body');
-
-
